3f56b07e8c55a19fe50b9d1187a9162c8226bbd9
qxou14 <qxou14@gmail.com>
2021-04-19 00:22:08 -0400

    Posting fixed

--- app/src/main/java/com/example/recommendationapp/MainActivity.java
+++ app/src/main/java/com/example/recommendationapp/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         // wrap File object into a content provider
         // required for API >= 24
         // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider.Place-Recommendation", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
